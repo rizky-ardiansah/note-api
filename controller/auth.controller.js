687889eeae49ef84deb360ab598519d1d7fb4b5b
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-03-18 17:53:31 +0700

    first commit

---
+++ controller/auth.controller.js
@@ -0,0 +1,7 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signUp = async () => { }
+export const login = async () => { }
+export const logout = async () => { }
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+import connectDB from './database/db.js';
+import authRoutes from './routes/auth.route.js';
+import noteRoutes from './routes/note.route.js';
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/notes',)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB()
+})
